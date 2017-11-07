fa10d108d80f9ef53218991bf7f3c3bba0a6e454
Catherine Callaghan <catherine.callaghan@exygy.com>
2017-11-07 15:26:04 -0800

    added ability to add custom classes to viz wrapper div

--- tableau-interactive.js
+++ tableau-interactive.js
@@ -79,7 +79,7 @@
 
         // Initialize the viz
         vizID = data.id + '_viz';
-        $viz = $('<div id="' + vizID + '" class="tableau-viz">');
+        $viz = $('<div id="' + vizID + '" class="tableau-viz ' + data.viz_wrapper_classes + '">');
         $vizWrapper.append($viz);
         var options = {device: $(window).width() < 768 ? 'phone' : 'desktop'};
         options.onFirstInteractive = function(e) {
